db93c2dec0273a01acaf0293edb016a03819cef5
Lukie Kang <Lukie Kang>
2020-02-09 17:16:08 +0000

    set up folders, environments, config

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+
+class ApplicationController < Sinatra::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        mime_type :css, 'text/css'
+        enable :sessions
+        set :session_secret, "superduperhypersecret"
+      end
+
+  get '/' do
+    'Hello, World!'
+  end
+
+end
