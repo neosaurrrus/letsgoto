fd7fbd09b538d2107a8f2731d3ffd400554b6aa6
Lukie Kang <Lukie Kang>
2020-02-13 13:48:25 +0000

    Add additional flash helpers

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,8 +13,13 @@ class UserController < ApplicationController
 
     post "/users/signup" do
         if User.find_by(username:params[:user][:username]) 
+            flash[:sign_in_notice]= "Username has been taken!"
+            redirect to("/users/signup")
+        elsif params[:user][:password].length < 8
+            flash[:sign_in_notice]= "Please use a minimum of 8 characters for the password!"
             redirect to("/users/signup")
         elsif params[:user][:password] != params[:password_confirm]
+            flash[:sign_in_notice]= "Passwords do not match!"
             redirect to("/users/signup")
         else
           @user = User.create(params[:user])
@@ -28,7 +33,7 @@ class UserController < ApplicationController
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             @attractions = Attraction.all
-            flash[:login_notice]= "Welcome #{@user.username}!"
+            flash[:index_notice] = "Welcome #{@user.username}!"
             redirect to("/attractions")
         else
          redirect to("/users/failure")
